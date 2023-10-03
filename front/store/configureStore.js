8402571910c9a7aa0fed28969688bb78e96792c0
Hyungchul You <teoyou881@gmail.com>
2023-10-02 23:54:17 -0400

    create configureStore

--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -1,9 +1,11 @@
-// configureStore.js
+const {createWrapper} = require('next-redux-wrapper');
 
 const configureStore=()=>{
 
 }
 
-const wrapper = createWrapper(configureStore);
+const wrapper = createWrapper(configureStore,{
+    debug:process.env.NODE_ENV === 'development'
+});
 
 export default wrapper;

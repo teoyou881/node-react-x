8ac5a80a7da5c9c595624b4c9d947c68b66beda6
Hyungchul You <teoyou881@gmail.com>
2023-10-04 16:32:30 -0400

    fix syntax error

--- front/pages/index.js
+++ front/pages/index.js
@@ -9,13 +9,15 @@ const Home = () => {
     const mainPosts = useSelector((state) => state.post.mainPosts);
     return (
         <AppLayout>
-			{isLoggedIn && < PostForm />}
-			{/* this is one of the anti-pattern that using index into key as props
+            {isLoggedIn && <PostForm />}
+            {/* this is one of the anti-pattern that using index into key as props
 				In most case, we must not pass index to key
 				especially, there is a possibility that post can be deleted.
 				But, if elements in iterator are not changed or deleted, can use index*/}
-			{/*{mainPosts.map((post,index)=> <PostCard key={index} post={post} />}*/}
-			{mainPosts.map((post,index)=> <PostCard key={post.id} post={post} />}
+            {/*{mainPosts.map((post,index)=> <PostCard key={index} post={post} />}*/}
+            {mainPosts.map((post, index) => (
+                <PostCard key={post.id} post={post} />
+            ))}
         </AppLayout>
     );
 };

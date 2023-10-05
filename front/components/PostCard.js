74d18304b53c20bcfe5bf935f101259a07812ccc
Hyungchul You <teoyou881@gmail.com>
2023-10-05 03:57:11 -0400

    build PostCard component

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -1,7 +1,74 @@
 import React from "react";
+import { Avatar, Button, Card, Popover } from "antd";
+import {
+    RetweetOutlined,
+    HeartOutlined,
+    MessageOutlined,
+    EllipsisOutlined,
+} from "@ant-design/icons";
+import ButtonGroup from "antd/lib/button/button-group";
+import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
+import PostImages from "./PostImages";
 
-const PostCard = () => {
-    return <div></div>;
+const PostCard = ({ post }) => {
+    const id = useSelector((state) => state.user.me?.id);
+    return (
+        <div style={{ marginBottom: 10 }}>
+            {/*<Image>*/}
+            {/*    <Content />*/}
+            {/*    <Buttons></Buttons>*/}
+            {/*</Image>*/}
+            <Card
+                cover={post.Images[0] && <PostImages images={post.Images} />}
+                // Key should be in jsx, when jsx is inside array.
+                actions={[
+                    <RetweetOutlined key="retweet" />,
+                    <HeartOutlined key="heart" />,
+                    <MessageOutlined key="comment" />,
+                    <Popover
+                        key="more"
+                        content={
+                            <ButtonGroup>
+                                {id && post.User.id === id ? (
+                                    <>
+                                        <Button>Edit</Button>
+                                        <Button type="danger">Delete</Button>
+                                    </>
+                                ) : (
+                                    <Button>Report</Button>
+                                )}
+                            </ButtonGroup>
+                        }
+                    >
+                        <EllipsisOutlined />
+                    </Popover>,
+                ]}
+            >
+                <Card.Meta
+                    avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
+                    title={post.User.nickname}
+                    description={post.content}
+                />
+            </Card>
+            {/*<CommentForm />*/}
+            {/*<Comments />*/}
+        </div>
+    );
+};
+
+PostCard.propTypes = {
+    post: PropTypes.shape({
+        id: PropTypes.number,
+        User: PropTypes.shape({
+            id: PropTypes.number,
+            nickname: PropTypes.string,
+        }),
+        content: PropTypes.string,
+        createAt: PropTypes.object,
+        Comments: PropTypes.arrayOf(PropTypes.object),
+        Images: PropTypes.arrayOf(PropTypes.object),
+    }),
 };
 
 export default PostCard;

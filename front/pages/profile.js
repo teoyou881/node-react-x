04467f5f8dfd6989b11dddc3b3d52bc38528e792
Hyungchul You <teoyou881@gmail.com>
2023-10-13 17:07:40 -0400

    If user don't log in yet, can't access to profile page

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppLayout from "../components/AppLayout";
 import Head from "next/head";
 import NicknameEditForm from "../components/NicknameEditForm";
 import FollowList from "../components/FollowList";
 import { useSelector } from "react-redux";
+import { useRouter } from "next/router";
 
 const Profile = () => {
     const { me } = useSelector((state) => state.user);
+    const router = useRouter();
+    useEffect(() => {
+        if (!me) router.push("/");
+    }, [me]); // Run this effect whenever 'me' changes
 
+    if (!me) {
+        return null;
+    }
     return (
         <>
             <Head>

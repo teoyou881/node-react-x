410e39685f9695c9649f005dd656cac24f067495
Hyungchul You <teoyou881@gmail.com>
2023-11-03 18:07:08 -0400

    refactor(Refactor NicknameEditForm): Refactor NicknameEditForm
    Refactor NicknameEditForm
    - Use Form and Input.Search from antd
    - Use
    custom hook useInput which i make
    - Reset empty after changing nickname

--- front/components/NicknameEditForm.js
+++ front/components/NicknameEditForm.js
@@ -1,6 +1,9 @@
-import { Form, Input } from 'antd';
-import React from 'react';
-import styled from 'styled-components';
+import { Form, Input } from "antd";
+import React, { useCallback } from "react";
+import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import { userAction } from "../reducers/user";
+import useInput from "../hooks/useInput";
 
 const DivWrapper = styled.div`
     margin-bottom: 20px;
@@ -9,12 +12,22 @@ const DivWrapper = styled.div`
 `;
 
 const NicknameEditForm = () => {
+    const [nickname, onChangeNickname, setNickname] = useInput("");
+    const dispatch = useDispatch();
+    const onSearch = useCallback(() => {
+        dispatch(userAction.changeNicknameRequest(nickname));
+        setNickname("");
+    }, [nickname]);
     return (
-        <DivWrapper>
-            <Form>
-                <Input.Search addonBefore='nickname' enterButton='edit' />
-            </Form>
-        </DivWrapper>
+        <Form>
+            <Input.Search
+                addonBefore="nickname"
+                enterButton="edit"
+                value={nickname}
+                onChange={onChangeNickname}
+                onSearch={onSearch}
+            />
+        </Form>
     );
 };
 

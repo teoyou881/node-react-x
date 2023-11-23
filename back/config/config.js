fcf3bd487c36abab28d620ccd702fb1bfaeb0cf2
Hyungchul You <teoyou881@gmail.com>
2023-11-23 05:27:23 -0500

    refactor(refactor): change db name
    -change db name nodeX

--- back/config/config.js
+++ back/config/config.js
@@ -10,7 +10,7 @@ module.exports = {
     development: {
         username: "root",
         password: process.env.DB_PASSWORD,
-        database: "react-node-x",
+        database: "nodeX",
         host: "127.0.0.1",
         port: "3306",
         dialect: "mysql",
@@ -18,14 +18,14 @@ module.exports = {
     test: {
         username: "root",
         password: null,
-        database: "database_test",
+        database: "nodeX_test",
         host: "127.0.0.1",
         dialect: "mysql",
     },
     production: {
         username: "root",
         password: null,
-        database: "database_production",
+        database: "nodeX_production",
         host: "127.0.0.1",
         dialect: "mysql",
     },

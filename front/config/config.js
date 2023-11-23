34ad50140358de2179dfeda6b2976ca0d34f5b94
Hyungchul You <teoyou881@gmail.com>
2023-11-23 04:21:37 -0500

    fix(fix): fix
    -fix backendUrl

--- front/config/config.js
+++ front/config/config.js
@@ -1,3 +1,3 @@
 // current back url
 // eslint-disable-next-line import/prefer-default-export
-export const backUrl = process.env.NODE_ENV === 'production' ? 'http://http://3.91.150.166' : 'http://localhost:3065';
+export const backUrl = process.env.NODE_ENV === 'production' ? 'http://3.91.150.166' : 'http://localhost:3065';

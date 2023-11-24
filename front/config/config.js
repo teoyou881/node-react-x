80b24f6c807c96414933369bb2c62bac1f34d93f
Hyungchul You <teoyou881@gmail.com>
2023-11-24 09:41:02 -0500

    refactor(refactor): change backurl to http://teonodex.com when NODE_ENV is production

--- front/config/config.js
+++ front/config/config.js
@@ -1,3 +1,3 @@
 // current back url
 // eslint-disable-next-line import/prefer-default-export
-export const backUrl = process.env.NODE_ENV === 'production' ? 'http://50.16.114.116' : 'http://localhost:3065';
+export const backUrl = process.env.NODE_ENV === 'production' ? 'http://teonodex.com' : 'http://localhost:3065';

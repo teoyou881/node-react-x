53ee1bce7ccfd09971d65936d204ed5a4ef5d870
Hyungchul You <teoyou881@gmail.com>
2023-11-27 14:06:41 -0500

    refactor(PostImages.js front/components): show images based on the number of images

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -1,7 +1,54 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { PlusOutlined } from '@ant-design/icons';
+import styled from 'styled-components';
 import ImagesZoom from './ImagesZoom';
+
+const Wrapper = styled.div`
+  width: calc(100% - 20px) !important;
+  height: 300px;
+  margin: 10px auto;
+`;
+
+const WrapperFor2 = styled.div`
+  width: calc(100% - 20px) !important;
+  height: 300px;
+  margin: 10px auto;
+  display: flex !important;
+  justify-content: space-around;
+
+  & > img {
+    width: 49%;
+    object-fit: cover;
+  }
+`;
+const WrapperFor3 = styled.div`
+  width: calc(100% - 20px) !important;
+  height: 300px;
+  margin: 10px auto;
+  display: flex !important;
+  justify-content: space-around;
+
+  & > div {
+    width: 49%;
+  }
+`;
+const WrapperFor4 = styled.div`
+  width: calc(100% - 20px) !important;
+  height: 300px;
+  margin: 10px auto;
+  display: flex !important;
+  flex-direction: column;
+  justify-content: space-around;
+  & > div {
+    height: 49%;
+  }
+`;
+
+const WrapperVer = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
 
 const PostImages = ({ images }) => {
   const [showImagesZoom, setShowImagesZoom] = useState(false);
@@ -21,76 +68,101 @@ const PostImages = ({ images }) => {
   }, []);
   if (images.length === 1) {
     return (
-      <div
-        style={{
-          height: firstMount ? '190.98px' : 'auto',
-          display: 'inline-block',
-        }}
-      >
+      <Wrapper>
         <img
           role="presentation"
-          style={{ width: '50%', display: 'inline-block' }}
+          style={{ width: '100%' }}
           src={`${images[0].src}`}
           alt={images[0].src}
           onClick={onZoom}
         />
         {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
-      </div>
+      </Wrapper>
     );
   }
   if (images.length === 2) {
     return (
-      <div
-        style={{
-          height: firstMount ? '190.98px' : 'auto',
-          display: 'inline-block',
-        }}
-      >
-        <img
-          role="presentation"
-          style={{ width: '50%', display: 'inline-block' }}
-          src={`${images[0].src}`}
-          alt={images[0].src}
-          onClick={onZoom}
-        />
-        <img
-          role="presentation"
-          style={{ width: '50%', display: 'inline-block' }}
-          src={`${images[1].src}`}
-          alt={images[1].src}
-          onClick={onZoom}
-        />
+      <WrapperFor2>
+        <img role="presentation" src={`${images[0].src}`} alt={images[0].src} onClick={onZoom} />
+        <img role="presentation" src={`${images[1].src}`} alt={images[1].src} onClick={onZoom} />
         {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
-      </div>
+      </WrapperFor2>
     );
   }
-  return (
-    <>
-      <img
-        role="presentation"
-        style={{ width: '50%', display: 'inline-block' }}
-        src={`${images[0].src}`}
-        alt={images[0].src}
-        onClick={onZoom}
-      />
-      <div
-        role="presentation"
-        style={{
-          display: 'inline-block',
-          width: '50%',
-          textAlign: 'center',
-          verticalAlign: 'middle',
-        }}
-        onClick={onZoom}
-      >
-        <PlusOutlined />
-        <br />
-        {images.length - 1}
-        more
-      </div>
-      {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
-    </>
-  );
+  if (images.length === 3) {
+    return (
+      <WrapperFor3>
+        <div>
+          <img
+            role="presentation"
+            src={`${images[0].src}`}
+            alt={images[0].src}
+            onClick={onZoom}
+            style={{ width: '100%', height: '100%' }}
+          />
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }}>
+          {' '}
+          <img
+            role="presentation"
+            src={`${images[1].src}`}
+            alt={images[1].src}
+            onClick={onZoom}
+            style={{ height: '49%' }}
+          />
+          <img
+            role="presentation"
+            src={`${images[2].src}`}
+            alt={images[2].src}
+            onClick={onZoom}
+            style={{ height: '49%' }}
+          />
+        </div>
+
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+      </WrapperFor3>
+    );
+  }
+  if (images.length === 4) {
+    return (
+      <WrapperFor4>
+        <WrapperVer>
+          <img
+            role="presentation"
+            src={`${images[0].src}`}
+            alt={images[0].src}
+            onClick={onZoom}
+            style={{ width: '49%' }}
+          />
+          <img
+            role="presentation"
+            src={`${images[1].src}`}
+            alt={images[1].src}
+            onClick={onZoom}
+            style={{ width: '49%' }}
+          />
+        </WrapperVer>
+        <WrapperVer>
+          <img
+            role="presentation"
+            src={`${images[2].src}`}
+            alt={images[2].src}
+            onClick={onZoom}
+            style={{ width: '49%' }}
+          />
+          <img
+            role="presentation"
+            src={`${images[3].src}`}
+            alt={images[3].src}
+            onClick={onZoom}
+            style={{ width: '49%' }}
+          />
+        </WrapperVer>
+
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+      </WrapperFor4>
+    );
+  }
 };
 
 PostImages.propTypes = {

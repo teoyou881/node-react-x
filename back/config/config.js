295382a1f6a5c0a7038d508f1a24b9b45c666826
Hyungchul You <teoyou881@gmail.com>
2023-11-29 07:24:30 -0500

    refactor(backend): follow eslint

--- back/config/config.js
+++ back/config/config.js
@@ -3,30 +3,30 @@
 // have to add module.exports to js
 //
 
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    development: {
-        username: "root",
-        password: process.env.DB_PASSWORD,
-        database: "nodeX",
-        host: "127.0.0.1",
-        port: "3306",
-        dialect: "mysql",
-    },
-    test: {
-        username: "root",
-        password: process.env.DB_PASSWORD,
-        database: "nodeX",
-        host: "127.0.0.1",
-        dialect: "mysql",
-    },
-    production: {
-        username: "root",
-        password: process.env.DB_PASSWORD,
-        database: "nodeX",
-        host: "127.0.0.1",
-        dialect: "mysql",
-    },
+  development: {
+    username: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'nodeX',
+    host: '127.0.0.1',
+    port: '3306',
+    dialect: 'mysql',
+  },
+  test: {
+    username: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'nodeX',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  production: {
+    username: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'nodeX',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
 };
--- back/models/comment.js
+++ back/models/comment.js
@@ -1,27 +1,27 @@
-const DataTypes = require("sequelize");
+const DataTypes = require('sequelize');
 const { Model } = DataTypes;
 
 module.exports = class Comment extends Model {
-    static init(sequelize) {
-        return super.init(
-            {
-                content: {
-                    type: DataTypes.TEXT,
-                    allowNull: false,
-                },
-            },
-            {
-                modelName: "Comment",
-                tableName: "comments",
-                charset: "utf8mb4",
-                collate: "utf8mb4_general_ci", // for saving emoji
-                sequelize,
-            },
-        );
-    }
+  static init(sequelize) {
+    return super.init(
+      {
+        content: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+        },
+      },
+      {
+        modelName: 'Comment',
+        tableName: 'comments',
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_general_ci', // for saving emoji
+        sequelize,
+      },
+    );
+  }
 
-    static associate(db) {
-        db.Comment.belongsTo(db.User);
-        db.Comment.belongsTo(db.Post);
-    }
+  static associate(db) {
+    db.Comment.belongsTo(db.User);
+    db.Comment.belongsTo(db.Post);
+  }
 };

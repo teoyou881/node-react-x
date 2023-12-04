a3d214bf224b65e730a9bca30a84db19e9b07999
Hyungchul You <teoyou881@gmail.com>
2023-12-04 15:54:13 -0500

    fix(front): fix a bug which ssr does not work. this is because antd. Go to antd and just follow docu.

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
+import { ConfigProvider } from 'antd';
 import wrapper from '../store/configureStore';
 
 const NodeX = ({ Component }) => (
-  <>
+  <ConfigProvider>
     <Head>
       <link rel="shortcut icon" href="/favicon.ico" />
       <meta charSet="utf-8" />
       <title>NodeX</title>
     </Head>
     <Component />
-  </>
+  </ConfigProvider>
 );
 
 NodeX.propTypes = {
--- front/pages/_document.tsx
+++ front/pages/_document.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs';
 
 export default class MyDocument extends Document {
   // eslint-disable-next-line consistent-return
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
+    const cache = createCache();
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(
+              <StyleProvider cache={cache}>
+                <App {...props} />
+              </StyleProvider>,
+            ),
         });
       const initialProps = await Document.getInitialProps(ctx);
+      const style = extractStyle(cache, true);
       return {
         ...initialProps,
         styles: (
           <>
             {initialProps.styles}
             {sheet.getStyleElement()}
+            <style dangerouslySetInnerHTML={{ __html: style }} />
           </>
         ),
       };
